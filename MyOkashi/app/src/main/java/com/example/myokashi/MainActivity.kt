0eff0f699c7792e2b9909a18e823293729d71ff5
LeoAndo <reo.ando@sun-asterisk.com>
2022-06-25 01:01:42 +0900

    MainActivityの修正

--- MyOkashi/app/src/main/java/com/example/myokashi/MainActivity.kt
+++ MyOkashi/app/src/main/java/com/example/myokashi/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.myokashi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.core.widget.doOnTextChanged
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonSearch = findViewById<Button>(R.id.buttonSearch)
+        val textInputLayoutKeyword = findViewById<TextInputLayout>(R.id.textInputLayoutKeyword)
+        val textInputEditTextKeyword =
+            findViewById<TextInputEditText>(R.id.textInputEditTextKeyword)
+
+        buttonSearch.isEnabled = false
+
+        textInputEditTextKeyword.doOnTextChanged { text, start, before, count ->
+            val isEnableButton = (count > 0)
+            val isErrorEnabled = (count == 0)
+            buttonSearch.isEnabled = isEnableButton
+            textInputLayoutKeyword.isErrorEnabled = isErrorEnabled
+            if (isErrorEnabled) {
+                textInputLayoutKeyword.error = "未入力です！"
+            }
+        }
+
+        buttonSearch.setOnClickListener {
+
+        }
     }
 }
