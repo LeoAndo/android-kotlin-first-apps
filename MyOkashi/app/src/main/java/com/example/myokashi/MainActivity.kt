cc18127b57c17318a2d7cdb437606460855810da
LeoAndo <reo.ando@sun-asterisk.com>
2022-07-02 16:37:36 +0900

    add usecase

--- MyOkashi/app/src/main/java/com/example/myokashi/MainActivity.kt
+++ MyOkashi/app/src/main/java/com/example/myokashi/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.myokashi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.Toast
 import androidx.activity.viewModels
