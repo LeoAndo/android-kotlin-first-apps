f1216588fe2d61270749017892c2de51f6c1eda4
LeoAndo <reo.ando@sun-asterisk.com>
2022-07-03 08:25:27 +0900

    init

--- CounterApp/app/src/main/java/jp/ac/jec/cm0199/counterapp/MainActivity.kt
+++ CounterApp/app/src/main/java/jp/ac/jec/cm0199/counterapp/MainActivity.kt
@@ -2,28 +2,10 @@ package jp.ac.jec.cm0199.counterapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val counter = Counter(0)
-        val txtCount = findViewById<TextView>(R.id.txtCount)
-
-        val buttonCountUp = findViewById<Button>(R.id.buttonCountUp)
-        buttonCountUp.setOnClickListener {
-            // ここにボタンクリックした時の処理を記述する！
-            counter.countUp()
-            txtCount.text = counter.value.toString()
-
-            if(counter.value > 10) {
-                SubActivity.startActivity(this)
-            }
-        }
     }
 }
-
-
