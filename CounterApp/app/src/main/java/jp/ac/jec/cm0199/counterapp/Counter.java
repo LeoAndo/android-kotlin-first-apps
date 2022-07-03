fd0740ed02ba61a80744bc4c61aab3274153e378
LeoAndo <reo.ando@sun-asterisk.com>
2022-07-03 10:12:04 +0900

    10KotlinからJavaの呼び出し パート1

---
+++ CounterApp/app/src/main/java/jp/ac/jec/cm0199/counterapp/Counter.java
@@ -0,0 +1,17 @@
+package jp.ac.jec.cm0199.counterapp;
+
+public final class Counter {
+    private int value;
+
+    public Counter(int value) {
+        this.value = value;
+    }
+
+    public void countUp() {
+        value++;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
--- CounterApp/app/src/main/java/jp/ac/jec/cm0199/counterapp/MainActivity.kt
+++ CounterApp/app/src/main/java/jp/ac/jec/cm0199/counterapp/MainActivity.kt
@@ -2,10 +2,22 @@ package jp.ac.jec.cm0199.counterapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val counter = Counter(0)
+        findViewById<Button>(R.id.buttonCountUp).setOnClickListener {
+            counter.countUp()
+            showToast(counter.value.toString())
+        }
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
