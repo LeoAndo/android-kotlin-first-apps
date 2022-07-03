2a134e8d0ee87b3d31748938044b0048ddadb911
LeoAndo <reo.ando@sun-asterisk.com>
2022-07-03 13:14:28 +0900

    add usecase

--- MyOkashi/app/src/main/java/com/example/myokashi/UiState.kt
+++ MyOkashi/app/src/main/java/com/example/myokashi/UiState.kt
@@ -1,8 +1,10 @@
 package com.example.myokashi
 
+import com.example.myokashi.domain.OkashiDomainModel
+
 sealed class UiState {
     object Initial : UiState()
     object Loading : UiState()
-    data class Success(val items: List<APIResponse.Item>?) : UiState()
+    data class Success(val items: List<OkashiDomainModel>?) : UiState()
     data class Error(val errorMessage: String) : UiState()
 }
